9bc5160bc8f17cd619b6c6bb23b24a6be3f17df2
chopperlee <lihengpro@gmail.com>
2017-06-23 18:27:28 +0800

    add tests

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
-import mysql from 'mysql2'
-// import { _ } from 'lodash'
+import mysql from 'mysql2/promise'
+import Promise from 'bluebird'
 
 export default class Json2mysql {
   constructor (opts) {
-    console.log('opts', opts)
-    this._connection = mysql.createConnection({
+    this._conn = mysql.createConnection({
       host: opts.host,
       user: opts.user,
+      password: opts.password,
       database: opts.database
     })
   }
 
-  async convert (tableName, jsonFile) {
-    console.log(tableName)
-    console.log(jsonFile)
-    // let updateSqls = []
-    // let insertSqls = []
-    const connection = await this._connection.connect()
-    console.log(connection)
+  decamelize (str) {
+    return str.replace(/[A-Z]/g, `_$1`)
+  }
 
-    if (Array.isArray(jsonFile)) {
-      for (let obj of jsonFile) {
-        if (obj.id) {
-          const [rows] = await connection.execute(`SELECT 1 FROM ${tableName} where id = ${obj.id}`)
-          console.log(rows)
-        }
-      }
-    }
+  // parase {name:'test',tableId:1} to 'name, table_id'
+  propsToColumn (obj) {
+    Object.keys(obj).reduce((pre, cur) => {
+      cur = this.decamelize(cur)
+      return `${pre}, ${cur}`
+    })
+  }
+
+  convert (tableName, jsonFile) {
+    const origins = require(jsonFile)
+    let _conn
+    return this._conn
+      .then(conn => {
+        _conn = conn
+        return _conn.query(`delete from ${tableName}`)
+      })
+      .then(() => _conn.query(`alter table ${tableName} auto_increment = 1`))
+      .then(() => Promise.mapSeries(origins, (d) => _conn.query(`insert into ${tableName} (${this.propsToColumn()}) values ('test' ,1)`)))
   }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai'
+import path from 'path'
 import Json2mysql from '../src'
 
 describe('json2mysql', () => {
@@ -11,6 +12,10 @@ describe('json2mysql', () => {
     }
     const j2m = new Json2mysql(opts)
     expect(j2m).to.be.an('object')
-    j2m.convert()
+    const file = path.join(__dirname, './user.json')
+    return j2m.convert('user', file)
+      .then(res => {
+        expect(res).to.be.an('object')
+      })
   })
 })
